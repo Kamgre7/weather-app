1ac63c6f49d1acbaf0c026ece01795af0d2d4a9d
Kamil <kamgre7@gmail.com>
2023-08-06 23:00:38 +0200

    Updated controller - using parsing from zod to take params

--- src/domains/weather/controllers/weatherController.ts
+++ src/domains/weather/controllers/weatherController.ts
@@ -1,103 +1,96 @@
 import { Request, Response } from 'express';
 import { IWeatherService, weatherService } from '../services/weatherService';
+import { GetCityParams } from '../schemas/getCitySchema';
+import { GetPredictionDayCityParams } from '../schemas/getPredictionDayCitySchema';
+import { GetPredictionCitiesParams } from '../schemas/getPredictionCitiesSchema';
+import { CoordinateSchema, DateSchema } from '../schemas/utilsSchemas';
 
 export interface IWeatherCityController {
-  // getCity(req: GetCityReq, res: Response): Promise<void>;
-  getByCity(req: Request, res: Response): Promise<void>;
-  //  getByCoordinates(req: GetCoordinateReq, res: Response): Promise<void>;
+  getCity(req: Request, res: Response): Promise<void>;
   getByCoordinates(req: Request, res: Response): Promise<void>;
-  // getByDayAndCity(req: GetPredictionDayCityReq, res: Response): Promise<void>;
   getPredictionByDayAndCity(req: Request, res: Response): Promise<void>;
-  //   getByBetweenDatesAndCity(req: GetPredictionCitiesReq, res: Response): Promise<void>;
   getPredictionByDatesAndCity(req: Request, res: Response): Promise<void>;
-  //  getPredictionByDatesAndCoordinates( req: Request,  res: Response): Promise<void>;
   getPredictionByDatesAndCoordinates(
     req: Request,
     res: Response
   ): Promise<void>;
 }
 
-export type FromToDates = {
-  from: string;
-  to: string;
-};
-
 export class WeatherCityController implements IWeatherCityController {
   constructor(private readonly weatherService: IWeatherService) {}
 
-  //getCity = async (req: GetCityReq, res: Response): Promise<void> => {
-  getByCity = async (req: Request, res: Response): Promise<void> => {
-    const { city } = req.params;
-    const apiKey = req.headers['x-weather-api-key'] as string;
+  getCity = async (req: Request, res: Response): Promise<void> => {
+    const weatherApiKey: string = res.locals.weatherApiKey;
+    const { city } = GetCityParams.parse(req.params);
 
-    const result = await this.weatherService.getByCity(city, apiKey);
+    const result = await this.weatherService.getByCity(city, weatherApiKey);
 
     res.status(200).json(result);
   };
 
-  //getByCoordinates = async (req: GetCoordinateReq, res: Response): Promise<void> => {
   getByCoordinates = async (req: Request, res: Response): Promise<void> => {
-    const coordinates = {
-      lat: parseFloat(req.params.lat),
-      lon: parseFloat(req.params.lon),
-    };
-    const apiKey = req.headers['x-weather-api-key'] as string;
+    const coordinates = CoordinateSchema.parse({
+      lat: req.params.lat,
+      lon: req.params.lon,
+    });
+    const weatherApiKey: string = res.locals.weatherApiKey;
 
     const result = await this.weatherService.getByCoordinates(
       coordinates,
-      apiKey
+      weatherApiKey
     );
 
     res.status(200).json(result);
   };
 
-  // getByDayAndCity(req: GetPredictionDayCityReq, res: Response): Promise<void>;
   getPredictionByDayAndCity = async (
     req: Request,
     res: Response
   ): Promise<void> => {
-    const { day, city } = req.params;
-    const apiKey = req.headers['x-weather-api-key'] as string;
+    const { day, city } = GetPredictionDayCityParams.parse(req.params);
+    const weatherApiKey: string = res.locals.weatherApiKey;
 
-    const result = await this.weatherService.getByDayAndCity(day, city, apiKey);
-
-    res.status(200).json(result);
-  };
-
-  // getByBetweenDatesAndCity(req: GetPredictionCitiesReq, res: Response): Promise<void>;
-  getPredictionByDatesAndCity = async (
-    req: Request,
-    res: Response
-  ): Promise<void> => {
-    const fromToDate = req.query as FromToDates;
-    const { city } = req.params;
-    const apiKey = req.headers['x-weather-api-key'] as string;
-
-    const result = await this.weatherService.getByDatesAndCity(
-      fromToDate,
+    const result = await this.weatherService.getByDayAndCity(
+      day,
       city,
-      apiKey
+      weatherApiKey
     );
 
     res.status(200).json(result);
   };
 
-  // getPredictionByDatesAndCoordinates( req: Request,  res: Response): Promise<void> =>
+  getPredictionByDatesAndCity = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    const fromToDate = DateSchema.parse(req.query);
+    const { city } = GetPredictionCitiesParams.parse(req.params);
+    const weatherApiKey: string = res.locals.weatherApiKey;
+
+    const result = await this.weatherService.getByDatesAndCity(
+      fromToDate,
+      city,
+      weatherApiKey
+    );
+
+    res.status(200).json(result);
+  };
+
   getPredictionByDatesAndCoordinates = async (
     req: Request,
     res: Response
   ): Promise<void> => {
-    const fromToDate = req.query as FromToDates;
-    const coordinates = {
-      lat: parseFloat(req.params.lat),
-      lon: parseFloat(req.params.lon),
-    };
-    const apiKey = req.headers['x-weather-api-key'] as string;
+    const fromToDate = DateSchema.parse(req.query);
+    const coordinates = CoordinateSchema.parse({
+      lat: req.params.lat,
+      lon: req.params.lon,
+    });
+    const weatherApiKey: string = res.locals.weatherApiKey;
 
     const result = await this.weatherService.getByDatesAndCoordinates(
       fromToDate,
       coordinates,
-      apiKey
+      weatherApiKey
     );
 
     res.status(200).json(result);
