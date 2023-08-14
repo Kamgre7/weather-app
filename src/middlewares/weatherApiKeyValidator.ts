1f05828c7b89c5f3822757bf64be7b81082dd7d0
Kamil <kamgre7@gmail.com>
2023-08-14 17:15:00 +0200

    Added UnauthorizedError and updated weatherApiKeyValidator

--- src/middlewares/weatherApiKeyValidator.ts
+++ src/middlewares/weatherApiKeyValidator.ts
@@ -2,38 +2,37 @@ import { NextFunction, Request, Response } from 'express';
 import { WeatherHeaderSchema } from '../domains/weather/schemas/utilsSchemas';
 import { z } from 'zod';
 import { weatherHttpClient } from '../httpClient/weatherHttpClient';
+import { UnauthorizedError } from '../errors/UnauthorizedError';
 
 export const weatherApiKeyValidator = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  let weatherApiKey: string;
+
   try {
     const headers = await WeatherHeaderSchema.parseAsync(req.headers);
 
-    const weatherApiKey = headers['x-weather-api-key'];
-
-    try {
-      await weatherHttpClient.get('/London/today', {
-        params: { key: weatherApiKey },
-      });
-
-      res.locals.weatherApiKey = weatherApiKey;
-      next();
-    } catch (err) {
-      return res.status(401).json({
-        status: 'failed',
-        error: 'Wrong x-weather-api-key value',
-      });
-    }
+    weatherApiKey = headers['x-weather-api-key'];
   } catch (error) {
     let err = error;
     if (err instanceof z.ZodError) {
       err = err.issues.map((e) => ({ path: e.path[0], message: e.message }));
     }
     return res.status(400).json({
-      status: 'failed',
       error: err,
     });
+  }
+
+  try {
+    await weatherHttpClient.get('/London/today', {
+      params: { key: weatherApiKey },
+    });
+
+    res.locals.weatherApiKey = weatherApiKey;
+    next();
+  } catch (err) {
+    throw new UnauthorizedError('Wrong x-weather-api-key');
   }
 };
