64287aec1c535e574a0d57352802dc5129cec5e0
Kamil <kamgre7@gmail.com>
2023-08-15 21:10:16 +0200

    Added weather service tests and added jest config dot env

---
+++ src/domains/weather/tests/weatherService.test.ts
@@ -0,0 +1,119 @@
+import nock, { Scope } from 'nock';
+import {
+  IWeatherApiAdapter,
+  WeatherApiAdapter,
+} from '../adapters/weatherApiAdapter';
+import { IWeatherService, WeatherService } from '../services/weatherService';
+import axios from 'axios';
+import { weatherApiData } from './weatherApiData';
+import { WeatherHttpClientSchema } from '../../../httpClient/weatherHttpClientSchemas';
+import { getFutureDateString } from './utils';
+
+describe('Weather service', () => {
+  let testHttpClient: string;
+  let scope: Scope;
+  let weatherApiAdapter: IWeatherApiAdapter;
+  let weatherService: IWeatherService;
+  let queryString: {
+    unitGroup: string;
+    key: string;
+    contentType: string;
+  };
+
+  let tomorrowDate: string;
+  let tenDaysLaterDate: string;
+
+  let dateRange: {
+    from: string;
+    to: string;
+  };
+
+  beforeAll(() => {
+    testHttpClient = WeatherHttpClientSchema.parse(
+      process.env.TEST_WEATHER_HTTP_CLIENT
+    );
+
+    tomorrowDate = getFutureDateString(1);
+    tenDaysLaterDate = getFutureDateString(10);
+
+    dateRange = {
+      from: tomorrowDate,
+      to: tenDaysLaterDate,
+    };
+
+    queryString = {
+      unitGroup: 'metric',
+      key: '123456',
+      contentType: 'json',
+    };
+
+    scope = nock(testHttpClient)
+      //getByCity
+      .get('/london/today')
+      .query(queryString)
+      .reply(200, weatherApiData)
+      // getByCoordinates
+      .get('/50,100/today')
+      .query(queryString)
+      .reply(200, weatherApiData)
+      //getByDatesAndCity
+      .get(`/london/${tomorrowDate}/${tenDaysLaterDate}`)
+      .query(queryString)
+      .reply(200, weatherApiData)
+      //getByDatesAndCoordinates
+      .get(`/50,100/${tomorrowDate}/${tenDaysLaterDate}`)
+      .query(queryString)
+      .reply(200, weatherApiData);
+
+    weatherApiAdapter = new WeatherApiAdapter(
+      axios.create({ baseURL: testHttpClient })
+    );
+
+    weatherService = new WeatherService(weatherApiAdapter);
+  });
+
+  it('Should return response from /city/today', async () => {
+    const result = await weatherService.getByCity('london', queryString.key);
+
+    expect(result).toStrictEqual(weatherApiData);
+  });
+
+  it('Should return response from /coordinates/today', async () => {
+    const coordinates = {
+      lat: 50,
+      lon: 100,
+    };
+
+    const result = await weatherService.getByCoordinates(
+      coordinates,
+      queryString.key
+    );
+
+    expect(result).toStrictEqual(weatherApiData);
+  });
+
+  it('Should return response from /city/dateRange', async () => {
+    const result = await weatherService.getByDatesAndCity(
+      dateRange,
+      'london',
+      queryString.key
+    );
+
+    expect(result).toStrictEqual(weatherApiData);
+  });
+
+  it('Should return response from /coordinates/dateRange', async () => {
+    const coordinates = {
+      lat: 50,
+      lon: 100,
+    };
+
+    const result = await weatherService.getByDatesAndCoordinates(
+      dateRange,
+      coordinates,
+      queryString.key
+    );
+
+    expect(result).toStrictEqual(weatherApiData);
+  });
+});
